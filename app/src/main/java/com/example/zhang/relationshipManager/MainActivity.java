0dea3e7ee68ca79ebdaa2cf14ad106ba828f0ca1
Huge <1004050187@qq.com>
2017-06-10 16:01:16 +0800

    testActivity

--- app/src/main/java/com/example/zhang/relationshipManager/MainActivity.java
+++ app/src/main/java/com/example/zhang/relationshipManager/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.zhang.relationshipManager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.zhang.relationshipManager.TestActivities.TestMainActivity;
+
+public class MainActivity extends BaseActivity {
+
+    public static void startActivity(Context context){
+        Intent intent=new Intent(context,MainActivity.class);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
